1cae64e283f0b8862766b936553960345d5d3b9f
ZenBogdan2604 <93253276+ZenBogdan2604@users.noreply.github.com>
2025-01-22 23:38:34 +0300

    add: some adaptive and burger-menu

--- attrac-project/src/components/header/Header.jsx
+++ attrac-project/src/components/header/Header.jsx
@@ -1,32 +1,55 @@
-import { NavLink } from 'react-router'
-import s from './Header.module.scss'
+import React, { useState } from 'react';
+import { NavLink } from 'react-router';
+import s from './Header.module.scss';
 
 export const Header = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
+    const closeMenu = () => {
+        setIsMenuOpen(false);
+    };
+
     return (
         <>
-        <header className={s.header}>
-        <div className={s.header__container}>
-            <div className={s.header__wrap}>
-                <div className={s.header__logo_block}>
-                    <NavLink to={""}><img src="./img/Group.svg" alt="" className={s.header__logo}/></NavLink>
-                    <NavLink to={""} className={s.header__name}>Toxin</NavLink>
+            <header className={s.header}>
+                <div className={s.header__container}>
+                    <div className={s.header__wrap}>
+                        <div className={s.header__logo_block}>
+                            <NavLink to={""}>
+                                <img src="./img/Group.svg" alt="" className={s.header__logo} />
+                            </NavLink>
+                            <NavLink to={""} className={s.header__name}>Toxin</NavLink>
+                        </div>
+                        <div className={s.header__menu}>
+                            <NavLink className={s.header__menu_btn} to={"./contacts"}>Контакты</NavLink>
+                            <NavLink className={s.header__menu_btn} to={"./attractions"}>Достопримечательности</NavLink>
+                        </div>
+                        <NavLink to={"./signin.html"} className={s.header__btn}>Войти</NavLink>
+                        <div
+                            className={`${s.header__menu_btn_burger} ${isMenuOpen ? s.active : ''}`}
+                            onClick={toggleMenu}
+                        >
+                            <span className={s.bar}></span>
+                            <span className={s.bar}></span>
+                            <span className={s.bar}></span>
+                        </div>
+                    </div>
                 </div>
-                <div>
-                    
-                </div>
-                <div className="header__menu">    
-                    <NavLink className={s.header__menu_btn} to={"./contacts"}>Контакты</NavLink>
-                    <NavLink className={s.header__menu_btn} to={"./attractions"}>Достопримечательности</NavLink>
-                </div>
-                <NavLink to={"./signin.html"} className={s.header__btn}>Войти</NavLink>
-                <div className={s.header__menu_btn_burger}>
-                    <span className={s.bar}></span>
-                    <span className={s.bar}></span>
-                    <span className={s.bar}></span>
+            </header>
+
+            {/* Бургер-меню */}
+            <div className={`${s.modal} ${isMenuOpen ? '' : s.hide}`}>
+                <div className={s.modalContent}>
+                    <label id={s.modalClose} onClick={closeMenu}>&#10006;</label>
+                    <NavLink className={s.modalLink} to={"./contacts"} onClick={closeMenu}>Контакты</NavLink>
+                    <NavLink className={s.modalLink} to={"./attractions"} onClick={closeMenu}>Достопримечательности</NavLink>
+                    <NavLink className={s.modalLink} to={"./signin.html"} onClick={closeMenu}>Войти</NavLink>
                 </div>
             </div>
-        </div>
-    </header>
         </>
-    )
-}
+    );
+};
